1017d38049c2223ce366378fa61c22fcafc3362b
poloyloy <aunzoselwyn0@gmail.com>
2025-05-20 14:36:43 +0800

    Add files via upload

--- calcu.js
+++ calcu.js
@@ -1,13 +1,40 @@
-const display = document.getElementById("display");
+const display = document.getElementById('display');
+const historyContainer = document.getElementById('history-container');
+let history = [];
 
-function appendToDisplay(input){
-    display.value += input;
+function appendToDisplay(value) {
+    display.value += value;
 }
 
-function clearDisplay(){
-    display.value = ""
+
+function clearDisplay() {
+    display.value = '';
 }
 
-function calculate(){
-    display.value = eval(display.value);
+function calculate() {
+    let expression = display.value.replace(/x/g, '*'); 
+    try {
+        let result = eval(expression);
+        display.value = result;
+
+     
+        let historyEntry = `${expression.replace(/\*/g, 'x')} = ${result}`;
+        history.push(historyEntry);
+        updateHistory();
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
+
+function updateHistory() {
+    if (history.length === 0) {
+        historyContainer.innerHTML = "There is no history yet...";
+        return;
+    }
+
+    historyContainer.innerHTML = '<ul class="list-group">';
+    history.forEach(entry => {
+        historyContainer.innerHTML += `<li class="list-group-item">${entry}</li>`;
+    });
+    historyContainer.innerHTML += '</ul>';
 }
